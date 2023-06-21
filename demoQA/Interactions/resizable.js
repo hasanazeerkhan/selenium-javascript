eb8d6793b0ee463356eb6895293bce9a6c6cf54b
Mohammed Hasan <hasanazeerkhan@gmail.com>
2023-06-21 12:56:15 +0530

    feat: new test

---
+++ demoQA/Interactions/resizable.js
@@ -0,0 +1,16 @@
+const {Builder, By} = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+const option = new chrome.Options();
+const driver = new Builder().forBrowser('chrome').setChromeOptions(option).build();
+
+async function resizable(){
+    try{
+        console.log('Executing tests');
+        await driver.get('');
+    }catch(error){
+        console.log("An error has occurred: "+error)
+    }finally{
+        console.log('Closing chrome...')
+    }
+}
+resizable();
