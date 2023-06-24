721e89088243c7dfd3c1dcc0515b1cfbd52484bd
Mohammed Hasan <hasanazeerkhan@gmail.com>
2023-06-24 15:54:23 +0530

    feat: steps for Alerts Frame Windows

--- demoQA/AlertsFrameWindows/frames.js
+++ demoQA/AlertsFrameWindows/frames.js
@@ -5,8 +5,10 @@ const driver = new Builder().forBrowser('chrome').setChromeOptions(option).build
 
 async function frames(){
     try{
-        console.log('Executing tests');
-        await driver.get('');
+        const url = "https://demoqa.com/frames";
+        console.log("Executing tests");
+        await driver.get(url);
+        await driver.manage().window().maximize();
     }catch(error){
         console.log("An error has occurred: "+error)
     }finally{
