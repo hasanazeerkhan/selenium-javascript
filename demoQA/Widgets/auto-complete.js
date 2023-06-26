b882a4d71d6a95c36669ad665ecede22991f14e2
Mohammed Hasan <hasanazeerkhan@gmail.com>
2023-06-26 19:09:12 +0530

    feat: test for Widgets

---
+++ demoQA/Widgets/auto-complete.js
@@ -0,0 +1,22 @@
+const { Builder, By } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+const option = new chrome.Options();
+const driver = new Builder()
+  .forBrowser("chrome")
+  .setChromeOptions(option)
+  .build();
+
+async function autoComplete() {
+  try {
+    const url = "https://demoqa.com/auto-complete";
+    console.log("Executing tests");
+    await driver.get(url);
+    await driver.manage().window().maximize();
+    console.log("Maximized screen");
+  } catch (error) {
+    console.log("An error has occurred: " + error);
+  } finally {
+    console.log("Closing chrome...");
+  }
+}
+autoComplete();
