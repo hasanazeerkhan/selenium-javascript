4605cd20d035e0c62d79f6253d4348786881b9db
Mohammed Hasan <hasanazeerkhan@gmail.com>
2023-07-05 19:42:33 +0530

    feat: test for droppable

--- demoQA/Interactions/droppable.js
+++ demoQA/Interactions/droppable.js
@@ -1,12 +1,20 @@
 const {Builder, By} = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
 const option = new chrome.Options();
+const action = new ActionSequence(driver);
 const driver = new Builder().forBrowser('chrome').setChromeOptions(option).build();
 
 async function droppable(){
     try{
         console.log('Executing tests');
-        await driver.get('');
+        const url = "https://demoqa.com/droppable";
+        await driver.get(url);
+        await driver.manage().window().maximize();
+        const sourceElement = await driver.findElement(By.xpath());
+        const targetElement = await driver.findElement(By.xpath());
+
+        await action.dragAndDrop(sourceElement, targetElement);
+
     }catch(error){
         console.log("An error has occurred: "+error)
     }finally{
